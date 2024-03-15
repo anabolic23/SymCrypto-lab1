bcc57d41c78a4cf2d207c66a5243f7b949152c15
anabolic23 <mishalopateckiy@gmail.com>
2024-03-15 12:20:09 +0200

    proper input.txt

--- SCrypto lab1/SCrypto lab1/main.cpp
+++ SCrypto lab1/SCrypto lab1/main.cpp
@@ -7,28 +7,28 @@ int main() {
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "");
 
-    Tools tool(true); 
+    Tools tool(false);
     tool.OpenFile("../input.txt");
-    
+
     tool.ProcessTextFile();
     tool.CountLetters();
     tool.CalculateLetterProbabilities();
 
     // Output the results
     std::wcout << L"\nLetter Frequencies:\n";
-    for (const auto& pair : tool.getLetterCounts()) { 
+    for (const auto& pair : tool.getLetterCounts()) {
         std::wcout << pair.first << L": " << pair.second << L"\n";
     }
 
     std::wcout << L"\nLetter Probabilities:\n";
-    for (const auto& pair : tool.getLetterProbabilities()) { 
+    for (const auto& pair : tool.getLetterProbabilities()) {
         std::wcout << pair.first << L": " << pair.second << L"\n";
     }
 
     double entropy1 = tool.CalculateEntropyH1();
     std::wcout << L"\nEntropy: " << entropy1 << L"\n";
 
-    tool.CountBigrams(2); 
+    tool.CountBigrams(2);
     tool.CalculateBigramsProbabilities();
 
     std::wcout << L"\nBigram Frequencies:\n";
